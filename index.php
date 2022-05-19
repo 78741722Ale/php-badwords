87dc49c25ad2e55eeb32d4dfea82d95d483f8870
78741722Ale <pecorillaalessandro@gmail.com>
2022-05-19 15:01:29 +0200

    Aggiunto ulteriori commenti e iniziato con nuove prove per ambientarmi in PHP

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 # Creare una variabile con un paragrafo di testo a vostra scelta.
-$parag = "Lorem ipsum dolor sit amet.";
+$parag = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore ex, eius cumque non officia error quam similique repudiandae amet, incidunt atque enim ipsa fugiat est!";
 
 # Stampare a schermo il paragrafo e la sua lunghezza.
 $par_length = strlen($parag); // la sua lunghezza la salvo in altro parametro
@@ -9,15 +9,20 @@ var_dump("Il testo del paragrafo è $parag, e la sua lunghezza è $par_length");
 # Una parola da censurare viene passata dall'utente tramite parametro GET.
 var_dump($_GET['bad']); // Modifica della Query
 $badWords = ($_GET['bad']); // la salvo in variabile
-var_dump("la brutta parola è $badWords "); // verifico in console
+var_dump("la brutta parola è $badWords"); // verifico in console
 
 #Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre 
 # asterischi (***) tutte le occorrenze della parola da censurare.
-
+/**
+ * $censor => la mia variabile
+ * str_replace() => la mia funzione per cambiare valore del mio paragrafo con badWords
+ * $badWords => la parola "filtrata" del mio paragrafo lorem
+ * '***' => cambio del valore di $badWords (come richiesto da consegna)
+ * $parag => nel mio paragrafo verrà filtrata la badWords e riceverà valore '***'
+ */
 $censor = str_replace($badWords, '***', $parag);
 $censor_length = strlen($censor_length); // la sua lunghezza la salvo in altro parametro
-var_dump("In console esce $censor e la sua lunghezza è $censor_length");
-
+var_dump("In console esce $censor e la sua lunghezza è $censor_length"); // Verifico ancora in console
 
 ?>
 
@@ -35,7 +40,9 @@ var_dump("In console esce $censor e la sua lunghezza è $censor_length");
     <p>Questo è il paragrafo di testo <em><?php echo $parag ?></em> e questà è la sua lunghezza <em><?php echo $par_length ?></em></p>
     <!-- Post Censura -->
     <h2>Post Censura</h2>
+    <!-- Stampo a schermo anche la brutta parola -->
     <p>La brutta parola inserita dall'utente è <em><?php echo $badWords ?></em></p>
+    <!-- Stampo a schermo nuovamente tutta la frase -->
     <p>Questo è il paragrafo di testo censurato <em><?php echo $censor ?></em> e questà è la sua lunghezza <em><?php echo $censor_length ?></em></p>
 </body>
 </html>
